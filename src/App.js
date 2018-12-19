b4c0bbb941b487ef1d54a2c8e1e50bd5802e596e
linchunshuang <18059451066@163.com>
2018-12-19 17:48:01 +0800

    fix: 引入HotModuleReplacementPlugin实现热更新

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
     render() {
         return (
-            <div>Hello ss111dt!222</div>
+            <div>Helweel222</div>
         );
     }
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,7 +2,12 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: path.resolve(__dirname, '../src/index.js'), //指定入口文件，程序从这里开始编译,__dirname当前所在目录, ../表示上一级目录, ./同级目录
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-dev-server/client?http://localhost:9090',
+        'webpack/hot/only-dev-server',
+        path.resolve(__dirname, '../src/index.js'), //指定入口文件，程序从这里开始编译,__dirname当前所在目录, ../表示上一级目录, ./同级目录
+    ],
     output: {
         path: path.resolve(__dirname, '../dist'), // 输出的路径
         filename: 'app/[name]_[hash:8].js'  // 打包后文件
@@ -20,6 +25,7 @@ module.exports = {
 　　 　　new HtmlWebpackPlugin({
 　　　　 　　template: path.resolve(__dirname, '../src/index.template.html'),
 　　　　　　 inject: true
-　　　　 })
+　　　　 }),
+        new webpack.HotModuleReplacementPlugin()
 　　 ]
 }
