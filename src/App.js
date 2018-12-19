3b2414e1c94fdcaf737ec53cac7688ce8c35b099
linchunshuang <18059451066@163.com>
2018-12-19 17:40:11 +0800

    fix: 引入webpack-dev-server，Html-webpack-plugin

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>Hello ss111dt!222</div>
+        );
+    }
+}
